637f7ba6561b1750dbf9f31f6b9148ee84305b6b
cmgiler <cmgiler@gmail.com>
2017-09-13 06:47:44 -0400

    feat: Begin work on social media web app clone.

---
+++ SocialMediaEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/HDD_Storage/version-control/Side-Projects/Social-Media-Website-Clone/SocialMediaEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
